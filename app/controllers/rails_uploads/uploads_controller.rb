f21dda474c1f23b520eb83bc3d9786c4929f093d
Mathías Montossi <mmontossi@mattways.com>
2013-02-04 01:34:32 -0200

    Really fix controller dash

--- app/controllers/rails_uploads/uploads_controller.rb
+++ app/controllers/rails_uploads/uploads_controller.rb
@@ -3,7 +3,7 @@ module RailsUploads
 
     def generate_image_preset
       filename = "#{params[:image]}.#{params[:format]}"
-      preset = params[:preset].gsub('-', '-').to_sym
+      preset = params[:preset].gsub('-', '_').to_sym
       if ::File.exists?(Rails.root.join('public', 'uploads', 'images', 'original', filename)) and Rails.application.config.uploads.presets.has_key?(preset)
         RailsUploads::Types::Image.new(filename).send :generate_preset, preset
         redirect_to request.url, :cb => Random.rand(100000)
