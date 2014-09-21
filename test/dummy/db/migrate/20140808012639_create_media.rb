87256806fec02300c5a872f29f4c8122d68ccc36
Mathías Montossi <mmontossi@museways.com>
2014-09-21 00:05:06 -0300

    Fix migration

--- test/dummy/db/migrate/20140808012639_create_media.rb
+++ test/dummy/db/migrate/20140808012639_create_media.rb
@@ -1,6 +1,6 @@
 class CreateMedia < ActiveRecord::Migration
   def change
-    create_table :medium do |t|
+    create_table :media do |t|
       t.attachment :attach
       t.attachment :local_attach
       t.attachment :s3_attach
