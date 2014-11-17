ecde9130b1710a721c07ac77dc21c391619e7971
Mathías Montossi <mmontossi@museways.com>
2014-11-17 14:01:15 -0200

    Add debug information

--- lib/attachs/attachment.rb
+++ lib/attachs/attachment.rb
@@ -101,7 +101,7 @@ module Attachs
         @upload = source
         @filename = source.original_filename.downcase
         @updated_at = Time.zone.now
-        Rails.logger.info "Setting time #{@updated_at}"
+        Rails.logger.info "Setting time #{(@updated_at.to_f * 10000000000).to_i}"
       end
       @content_type = source.content_type
       @size = source.size
